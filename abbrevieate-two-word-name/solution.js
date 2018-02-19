6c52ce3caa409040a65819cfe56747f2494acb32
Peter Kim <seattlechem@yahoo.com>
2018-02-19 13:26:37 -0800

    implement .toUpperCase()

--- abbrevieate-two-word-name/solution.js
+++ abbrevieate-two-word-name/solution.js
@@ -1,11 +1,11 @@
 let name = 'Peter Chan';
 function abbrevName(name){
-  let first = name[0];
+  let first = name[0].toUpperCase();
   let second;
   let abbrev;
   for (let i in name) {
     if(name[i] === ' '){
-      second = name[parseInt(i) + 1];
+      second = name[parseInt(i) + 1].toUpperCase();
       abbrev = `${first}.${second}`;
     }
   }
