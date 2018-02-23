9cc4060c18462665e1453ab6461ceb4a879b612d
Peter Kim <seattlechem@yahoo.com>
2018-02-23 13:19:00 -0800

    checking for null is added

--- 08-formatwords/solution.js
+++ 08-formatwords/solution.js
@@ -1,5 +1,5 @@
 function formatWords (words) {
-  if (!words.length) return '';
+  if (!words.length || words.length === null) return '';
   words = words.filter(function(n){ return n !== ''; });
   return words.map(function(a, i){ if (i === words.length-2) return a + ' and'; return a + ','; }).join(' ').slice(0, -1);
 }
